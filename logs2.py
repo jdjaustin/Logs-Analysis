1006fdcd36eb3e466314be602c3e07e2f861ce33
jdjaustin <jdjaustin@gmail.com>
2019-02-25 19:01:36 -0600

    Delete logs2.py

--- logs2.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn=psycopg2.connect(dbname="news")
-cursor=conn.cursor()
-cursor.execute("select authors.name, count(*) as num from articles join authors on articles.author=authors.id join log on log.path=concat('/article/',articles.slug) group by authors.name order by num desc limit 3")
-results=cursor.fetchall()
-print results
-conn.close()
