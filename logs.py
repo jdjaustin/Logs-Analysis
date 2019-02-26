b90eb5c70e647e85fa6ee05677849f74238538b8
jdjaustin <jdjaustin@gmail.com>
2019-02-25 19:01:43 -0600

    Delete logs.py

--- logs.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-conn=psycopg2.connect(dbname="news")
-cursor=conn.cursor()
-cursor.execute("select title, count(*) as num from articles,log where log.path like concat('/article/',articles.slug) group by articles.title order by num desc limit 3")
-results=cursor.fetchall()
-print results
-conn.close()
